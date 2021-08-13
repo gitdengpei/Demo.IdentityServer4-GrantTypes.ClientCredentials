56208b68fd52dc2e37da207a9899bf50f61072c1
gitdengpei <1451554820@qq.com>
2021-08-13 21:40:26 +0800

    添加项目文件。

---
+++ Demo.IdentityServer4/Config.cs
@@ -0,0 +1,39 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Demo.IdentityServer4
+{
+	public static class Config
+	{
+		/// <summary>
+		/// api资源列表
+		/// </summary>
+		/// <returns></returns>
+		public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+		{
+			new  ApiResource("DemoScope", "DemoScope Service")
+		};
+		/// <summary>
+		/// 4.0版本需要添加apiscope
+		/// </summary>
+		/// <returns></returns>
+		public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+		{
+			new ApiScope("DemoScope","DemoScope")
+		};
+		public static IEnumerable<Client> Clients => new Client[]
+		{
+			new Client
+			{
+				ClientId = "DemoScope_client",
+				ClientName = "DemoScope_Client",
+				ClientSecrets = { new Secret("secret".Sha256()) },
+				AllowedGrantTypes = GrantTypes.ClientCredentials,
+				AllowedScopes = {
+					"DemoScope"
+				}
+			}
+
+		};
+	}
+}
---
+++ Demo.IdentityServer4/Startup.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Demo.IdentityServer4
+{
+	public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = services.AddIdentityServer();
+            builder.AddDeveloperSigningCredential();//生成的密钥将被保存到文件系统，以便在服务器重新启动之间保持稳定（可以通过传递来禁用false）
+            builder.AddInMemoryClients(Config.Clients);
+            builder.AddInMemoryApiScopes(Config.ApiScopes);
+            builder.AddInMemoryApiResources(Config.ApiResources);
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+            app.UseIdentityServer();
+        }
+    }
+}
